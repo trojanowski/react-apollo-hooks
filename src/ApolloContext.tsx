b4c7b48a84c5eb189429d374fb4e29f616edad71
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-28 14:44:29 +0500

    [WIP]

--- src/ApolloContext.tsx
+++ src/ApolloContext.tsx
@@ -1,8 +1,6 @@
 import ApolloClient from 'apollo-client';
 import React, { ReactElement, ReactNode, useContext } from 'react';
 
-import { assertApolloClient } from './utils';
-
 const ApolloContext = React.createContext<null | ApolloClient<any>>(null);
 
 export interface ApolloProviderProps<TCacheShape> {
@@ -21,12 +19,16 @@ export function ApolloProvider<TCacheShape = any>({
   );
 }
 
-export function useApolloClient<TCache = object>(): null | ApolloClient<
-  TCache
-> {
+export function useApolloClient<TCache = object>(): ApolloClient<TCache> {
   const client = useContext(ApolloContext);
 
-  assertApolloClient(client);
-
+  if (!client) {
+    // https://github.com/apollographql/react-apollo/blob/5cb63b3625ce5e4a3d3e4ba132eaec2a38ef5d90/src/component-utils.tsx#L19-L22
+    throw new Error(
+      'Could not find "client" in the context or passed in as a prop. ' +
+        'Wrap the root component in an <ApolloProvider>, or pass an ' +
+        'ApolloClient instance in via props.'
+    );
+  }
   return client;
 }
