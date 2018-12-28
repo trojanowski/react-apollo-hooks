c771dc6692a4e2802e405d16bc041c5307c22830
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-28 14:38:29 +0500

    [WIP]

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,8 @@ import {
   getCachedObservableQuery,
   invalidateCachedObservableQuery,
 } from './queryCache';
-import { Omit, assertApolloClient } from './utils';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 const ApolloContext = React.createContext<null | ApolloClient<any>>(null);
 
@@ -51,9 +52,16 @@ export function ApolloProvider<TCacheShape = any>({
 export function useApolloClient<TCache = object>(): ApolloClient<TCache> {
   const client = useContext(ApolloContext);
 
-  assertApolloClient(client);
+  if (!client) {
+    // https://github.com/apollographql/react-apollo/blob/5cb63b3625ce5e4a3d3e4ba132eaec2a38ef5d90/src/component-utils.tsx#L19-L22
+    throw new Error(
+      'Could not find "client" in the context or passed in as a prop. ' +
+        'Wrap the root component in an <ApolloProvider>, or pass an ' +
+        'ApolloClient instance in via props.'
+    );
+  }
 
-  return client!;
+  return client;
 }
 
 export interface QueryHookOptions<TVariables>
