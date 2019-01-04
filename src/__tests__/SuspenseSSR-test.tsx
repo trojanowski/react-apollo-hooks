5c97b53338efa506e83277ff8c7fe4e6fb155c53
Umidbek Karimov <uma.karimov@gmail.com>
2019-01-04 14:47:24 +0500

    [WIP]

---
+++ src/__tests__/SuspenseSSR-test.tsx
@@ -0,0 +1,133 @@
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { MockedResponse } from 'apollo-link-mock';
+import gql from 'graphql-tag';
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+import { render } from 'react-testing-library';
+
+import { ApolloProvider } from '../ApolloContext';
+import { unstable_SuspenseSSR as SuspenseSSR } from '../SuspenseSSR';
+import createClient from '../__testutils__/createClient';
+import flushEffectsAndWait from '../__testutils__/flushEffectsAndWait';
+import { getMarkupFromTree } from '../getMarkupFromTree';
+import { QueryHookOptions, useQuery } from '../useQuery';
+
+const USER_QUERY = gql`
+  {
+    currentUser {
+      firstName
+    }
+  }
+`;
+
+interface UserQueryResult {
+  currentUser: { firstName: string };
+}
+
+const MOCKS: MockedResponse[] = [
+  {
+    request: { query: USER_QUERY },
+    result: { data: { currentUser: { firstName: 'James' } } },
+  },
+];
+
+function createMockClient(link?: ApolloLink) {
+  return createClient({ link, mocks: MOCKS, addTypename: false });
+}
+
+function UserDetails(props: QueryHookOptions<{}>) {
+  const { data, loading } = useQuery<UserQueryResult>(USER_QUERY, props);
+
+  return (
+    <div>
+      {loading || !data || !data.currentUser
+        ? 'Loading user details'
+        : data.currentUser.firstName}
+    </div>
+  );
+}
+
+interface UserWrapperProps extends QueryHookOptions<{}> {
+  readonly client: ApolloClient<object>;
+}
+
+function UserDetailsWrapper({ client, ...props }: UserWrapperProps) {
+  return (
+    <ApolloProvider client={client}>
+      <SuspenseSSR fallback={<>Loading with suspense</>}>
+        <UserDetails {...props} />
+      </SuspenseSSR>
+    </ApolloProvider>
+  );
+}
+
+it('not throws in react-dom with suspense', async () => {
+  const client = createMockClient();
+
+  const { container } = render(<UserDetailsWrapper client={client} suspend />);
+
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  Loading with suspense
+</div>
+`);
+
+  await flushEffectsAndWait();
+
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div>
+    James
+  </div>
+</div>
+`);
+});
+
+it('not throws in react-dom without suspense', async () => {
+  const client = createMockClient();
+
+  const { container } = render(
+    <UserDetailsWrapper client={client} suspend={false} />
+  );
+
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div>
+    Loading user details
+  </div>
+</div>
+`);
+
+  await flushEffectsAndWait();
+
+  expect(container).toMatchInlineSnapshot(`
+<div>
+  <div>
+    James
+  </div>
+</div>
+`);
+});
+
+it('not throws in react-dom/server with suspense', async () => {
+  const client = createMockClient();
+
+  await expect(
+    getMarkupFromTree({
+      renderFunction: renderToString,
+      tree: <UserDetailsWrapper client={client} suspend />,
+    })
+  ).resolves.toMatchInlineSnapshot(`"<div>James</div>"`);
+});
+
+it('not throws in react-dom/server without suspense', async () => {
+  const client = createMockClient();
+
+  await expect(
+    getMarkupFromTree({
+      renderFunction: renderToString,
+      tree: <UserDetailsWrapper client={client} suspend={false} />,
+    })
+  ).resolves.toMatchInlineSnapshot(`"<div>James</div>"`);
+});
