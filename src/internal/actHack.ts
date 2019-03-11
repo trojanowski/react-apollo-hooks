5b7804d30f16fb3975e2513be674ade4a3a12f58
Daniel K <FredyC@users.noreply.github.com>
2019-03-11 15:40:25 +0100

    Update actHack.ts

--- src/internal/actHack.ts
+++ src/internal/actHack.ts
@@ -1,6 +1,7 @@
 import { act } from 'react-dom/test-utils';
 
 export default function actHack(callback: (() => void)) {
+  // @ts-ignore process is not known
   if (process.env.NODE_ENV === 'test') {
     act(callback);
   } else {
